693ad802bdb091c8fb38d716f3dd2af67f68bc28
jho4n-g <velasco9802@gmail.com>
2026-05-13 11:05:24 -0400

    env

--- src/middlewares/validateSchema.middlewares.js
+++ src/middlewares/validateSchema.middlewares.js
@@ -7,17 +7,10 @@ export const validateSchema = (schema) => (req, res, next) => {
     next();
   } catch (error) {
     if (error instanceof ZodError) {
-      const detailedErrors = {};
-
-      for (const issue of error.issues) {
-        const path = issue.path.reduce((acc, key) => {
-          if (typeof key === 'number') return `${acc}[${key}]`;
-          return acc ? `${acc}.${key}` : key;
-        }, '');
-
-        if (!detailedErrors[path]) detailedErrors[path] = [];
-        detailedErrors[path].push(issue.message);
-      }
+      const detailedErrors = error.issues.map((issue) => ({
+        field: issue.path.join('.'),
+        message: issue.message,
+      }));
       console.log(detailedErrors);
       return res.status(400).json({
         ok: false,
